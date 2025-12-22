b94574d891cb518e2d6c70e4fc23e4fa83d5df5e
Johann Schneider <joschi.dev7@gmail.com>
2025-12-22 21:09:25 +0100

    gone to the next challenge, created new structures and components.

--- first-react/src/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-      <footer className='footer'>
-        <small className='footer-item'>© 2025 React Facts. All rights reserved.</small>
-      </footer>
-  )
-}
---
+++ first-react/src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+export default function Navbar() {
+    return (
+        <header>
+            <nav>
+                <img src= "/src/assets/react.svg" className="react-logo" alt="React logo" />
+                <span>React Facts</span>
+            </nav>
+        </header>
+    )
+}
